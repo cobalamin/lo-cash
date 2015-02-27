8ada734aea2469c85a6f8f2401c798c0a1853aac
Simon Welker <simon.welker@esirion.de>
2015-02-27 13:28:11 +0100

    Add JS object selection helpers to core

--- src/lo_cash/core.cljs
+++ src/lo_cash/core.cljs
@@ -1,8 +1,13 @@
-(ns lo-cash.core
-  (:require [clojure.browser.repl :as repl]))
+(ns lo-cash.core)
 
-;; (repl/connect "http://localhost:9000/repl")
+(defn obj-select
+  "Selects the given keys from a JS object and returns a keywordized map"
+  [obj [& keys]]
+  (into {}
+    (map #([(keyword k) (aget obj k)])
+         keys)))
 
-(enable-console-print!)
-
-(println "Hello world!")
+(defn obj-selectv
+  "Selects the given keys from a JS object and returns a vector of their values"
+  [obj [& keys]]
+  (mapv #(aget obj k) keys))
